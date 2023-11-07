4e459528d31414b7e581b5a542f87aa83873affd
yovel <ykey-cohen@emeryweiner.org>
2023-11-07 06:41:14 -0600

    lab4

--- Lab 4/graph.py
+++ Lab 4/graph.py
@@ -10,8 +10,18 @@ photopeaks = {
   'co60': (1173, 1332),
   'co57': (706, 137),
   'ba133': (437, 382),
-  'cs127': (661,),
-  'cd109': (),
+  'cs137': (661,),
+  'cd109': (203, 260),
+}
+
+real_photopeaks = {
+  'na22': (511, 1274),
+  'mn54': (851,),
+  'co60': (1192, 1356),
+  'co57': (141, 666),
+  'ba133': (356,),
+  'cs137': (675,),
+  'cd109': (675,),
 }
 
 for file in files:
@@ -24,9 +34,28 @@ for file in files:
     for E in photopeaks[fname]:
         plot.axvline(x=E, color='red', linewidth=0.5)
     plot.scatter(data[1], data[0], s=2)
+    print((data[1][1]-data[1][0]))
+    #plot.scatter(data[1], -np.sqrt(data[0])*np.gradient(np.gradient(data[0]))/(data[1][1]-data[1][0]), s=1)
+    print(list(data[1]))
+    MIN = None
+    MAX = None
+    for p in real_photopeaks[fname]:
+        peaki = np.argmin(abs(data[1]-p))
+        for i, _ in enumerate(reversed(data[0][:peaki+1])):
+            if fname == 'na22': print(data[0][:peaki+1])
+            if data[0][peaki-i] > data[0][peaki-i+1]:
+                MIN = data[1][peaki-i+1]
+            if MIN and data[0][peaki-i] < data[0][peaki-i+1]:
+                MAX = data[1][peaki-i+1]
+                break
+        print(MIN, MAX)
+        plot.axvline(x=MIN/2+MAX/2, color='green', linewidth=0.5)
 
-    plot.xlim([0, 1500])
     plot.ylabel('Counts')
     plot.xlabel('Energy (keV)')
+    #plot.xticks(data[1][data[1]<1500])
+    plot.xlim([0, 1500])
     plot.title('Number of Events by Energy (%s)' % element)
-    plot.savefig('images/%s.png' % fname)
+    #plot.savefig('images/%s.png' % fname)
+
+plot.show()
